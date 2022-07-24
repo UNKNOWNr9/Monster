585843842d4b4f647decc4fc6317aa12293f9b52
UNKOWNr9 <arshia21420@gmail.com>
2022-07-24 09:19:31 -0700

    1

--- Nasa/urls.py
+++ Nasa/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('contact', views.Contact.as_view(), name='contact'),
     path('post/', views.Post.as_view(), name='post'),
     path('Nasa/<slug:slug>', views.Nasa_Detail, name='Nasa-Detail'),
+    path('search/', views.SearchBar.as_view(), name='search'),
+    path('search/page/<int:page>', views.SearchBar.as_view(), name='search'),
 ]
--- Nasa/views.py
+++ Nasa/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from Nasa.models import post
+from django.db.models import Q
 # Create your views here.
 class Home(ListView):
     queryset = ''
@@ -25,3 +26,16 @@ def Nasa_Detail(request, slug):
         'obj' : get_object_or_404(post, Slug=slug, Status="p")
     }
     return render(request, 'WebSite/Nasa-Detail.html', context)
+
+class SearchBar(ListView):
+    template_name = 'WebSite/search-bar.html'
+    paginate_by = 5
+
+    def get_queryset(self):
+        search = self.request.GET.get('q')
+        return post.objects.filter(Q(Description__icontains=search) | Q(Title__icontains=search))
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['search'] = self.request.GET.get('q')
+        return context
